7696744297c2debfed2a1255f3091434dde50c2f
37sprinkles <169003917+37sprinkles@users.noreply.github.com>
2024-05-06 19:26:40 -0400

    feat: Simpified cli args

--- main.go
+++ main.go
@@ -1,106 +1,68 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/jaevor/go-nanoid"
 )
 
-func parseFlags() (string, int) {
-	var (
-		charset string
-		hex     bool
-		alpha   bool
-		numeric bool
-		base64  bool
-		base90  bool
-	)
-
-	flag.StringVar(&charset, "c", "", "Custom character set")
-	flag.BoolVar(&hex, "hex", false, "Uses a hexadecimal character set")
-	flag.BoolVar(&alpha, "alpha", false, "Uses an alphabetic character set")
-	flag.BoolVar(&numeric, "numeric", false, "Uses a numeric character set")
-	flag.BoolVar(&base64, "base64", false, "Uses a base64 character set")
-	flag.BoolVar(&base90, "base90", false, "Uses a base90 character set (includes symbols)")
-
-	flag.Usage = func() {
-		fmt.Printf("Usage: nanoid [options] length\nOptions:\n")
+func parseFlags() (string, int, int) {
+	printUsage := func() {
+		fmt.Println("Usage: nanoid charset length [count]")
+		fmt.Println("  charset (string): required, \"hex\", \"alpha\", \"numeric\", \"base64\", \"base90\" or a string of characters")
+		fmt.Println("  length (byte): required, must be between 2 and 255")
+		fmt.Println("  count (int32): optional, must be at least 1")
 		flag.PrintDefaults()
 		os.Exit(0)
 	}
 
 	flag.Parse()
 
-	if len(flag.Args()) != 1 {
-		panic(errors.New("invalid args: missing length"))
+	if len(flag.Args()) < 2 || len(flag.Args()) > 3 {
+		printUsage()
 	}
 
-	length, err := strconv.Atoi(flag.Arg(0))
-	if err != nil {
-		panic(errors.New("invalid args: length must be an integer"))
-	}
-
-	if length == 0 {
-		panic(errors.New("invalid args: length cannot be 0"))
-	}
-
-	if length <= 0 || length > 255 {
-		panic(errors.New("invalid args: length must be greater that 0 and less that 256"))
-	}
-
-	if hex {
-		if charset != "" {
-			panic(errors.New("invalid args: multiple character sets specified"))
-		}
-
+	charset := strings.ToLower(flag.Arg(0))
+	switch charset {
+	case "hex":
 		charset = "0123456789ABCDEF"
-	}
-
-	if alpha {
-		if charset != "" {
-			panic(errors.New("invalid args: multiple character sets specified"))
-		}
-
+	case "alpha":
 		charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	}
-
-	if numeric {
-		if charset != "" {
-			panic(errors.New("invalid args: multiple character sets specified"))
-		}
-
+	case "numeric":
 		charset = "0123456789"
 	}
 
-	if base64 {
-		if charset != "" {
-			panic(errors.New("invalid args: multiple character sets specified"))
+	length, err := strconv.Atoi(flag.Arg(1))
+	if err != nil {
+		printUsage()
+	}
+
+	if length <= 1 || length > 255 {
+		printUsage()
+	}
+
+	count := 1
+	if len(flag.Args()) == 3 {
+		count, err = strconv.Atoi(flag.Arg(2))
+
+		if err != nil {
+			printUsage()
 		}
-
-		charset = "base64"
 	}
 
-	if base90 {
-		if charset != "" {
-			panic(errors.New("invalid args: multiple character sets specified"))
-		}
-
-		charset = "base90"
+	if count <= 0 {
+		printUsage()
 	}
 
-	if charset == "" {
-		panic(errors.New("invalid args: missing character set"))
-	}
-
-	return charset, length
+	return charset, length, count
 }
 
 func main() {
-	charset, length := parseFlags()
+	charset, length, count := parseFlags()
 
 	var generator func() string
 	var err error
@@ -117,5 +79,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(generator())
+	for range count {
+		fmt.Println(generator())
+	}
 }
